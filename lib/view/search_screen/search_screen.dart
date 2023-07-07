9614bc91528b72a58f6273da0aa2cbbdb620edc5
Harshil Gajipara <harshilgajipara5@gmail.com>
2023-07-07 14:21:22 +0530

    7/7/2023

--- lib/view/search_screen/search_screen.dart
+++ lib/view/search_screen/search_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_practice/res/constant/app_string.dart';
+import 'package:getx_practice/utils/routes/app_routes.dart';
+import 'package:getx_practice/utils/routes/routes_name.dart';
 
 class SearchScreen extends GetView<SearchController> {
   const SearchScreen({super.key});
@@ -21,6 +23,19 @@ class SearchScreen extends GetView<SearchController> {
               },
               child: Text(
                 "Back To Home Screen",
+              ),
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              onPressed: () {
+                Get.offNamed(RoutesName.homeScreen)
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Replace Screen",
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
           ],
